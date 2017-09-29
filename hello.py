86855b52282f5bb66d9626bd67c016ed6bd343eb
Elad Katz <eladkt@gmail.com>
2017-09-29 13:48:06 +0300

    first basic version of calculator

---
+++ hello.py
@@ -0,0 +1,10 @@
+"""learning python"""
+from __future__ import print_function
+
+print ("Hello, World!")
+print ("Hello, Nick!")
+print ("Hello, Maayan!")
+
+X = raw_input('Enter your name: ')
+
+print ('hello ', X)
